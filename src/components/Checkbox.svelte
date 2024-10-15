6966bc91a98ca42762fcfa40d06b50c1923d2273
Zeu Capua <coding@zeu.dev>
2024-10-14 22:28:12 -0700

    implement checkbox svelte component

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let isChecked = $state(false);
+  let description = $state("");
+</script>
+
+<span class="flex gap-2">
+  <input type="checkbox" bind:checked={isChecked} />
+  <input type="text" bind:value={description} class="border-b" />
+</span>
